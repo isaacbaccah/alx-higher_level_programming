bfc84e28937c8640725909c2f8349d8ef1ee8399
isaacbaccah <32774186+isaacbaccah@users.noreply.github.com>
2022-08-25 07:37:49 +0000

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-# 100-my_calculator.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
