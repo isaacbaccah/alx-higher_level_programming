3c3313a0931963bbbe465ca73a9a8b15599ad02a
isaacbaccah <32774186+isaacbaccah@users.noreply.github.com>
2022-08-26 07:24:23 +0000

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# 6-print_matrix_integer.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers."""
     for i in range(len(matrix)):
